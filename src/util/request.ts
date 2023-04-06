e0736993e8b586770ba901d3d58a7fd87d740bda
zNotChill <znotchill@znci.dev>
2023-04-06 19:30:22 +0100

    bro ffs
    Signed-off-by: zNotChill <znotchill@znci.dev>

--- src/util/request.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import * as dotenv from "dotenv";
-dotenv.config();
-
-export const request = (url: string, { method = "GET", data}) => new Promise(async (resolve, reject) => {
-
-});
